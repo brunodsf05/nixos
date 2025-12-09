59e3bf4ce8a6a0aaa6c7ba27d2ed5c61a31a0209
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-12-09 22:43:17 +0000

    Refactor: Function to generate a "nixosSystem"

--- flake.nix
+++ flake.nix
@@ -9,18 +9,31 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, home-manager, ... }: {
-    nixosConfigurations = {
-      wsl = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          nixos-wsl.nixosModules.default
-          home-manager.nixosModules.home-manager
-          { networking.hostName = "wsl"; }
-          ./modules/home
-          ./modules/system.nix
-        ];
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    nixos-wsl,
+    home-manager,
+    ...
+  }:
+  let
+    mkHost = host: system: nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {
+        inherit inputs;
       };
+      modules = [
+        nixos-wsl.nixosModules.default
+        home-manager.nixosModules.home-manager
+        { networking.hostName = host; }
+        ./modules/home
+        ./modules/system.nix
+      ];
+    };
+  in
+  {
+    nixosConfigurations = {
+      wsl = mkHost "wsl" "x86_64-linux";
     };
   };
 }
