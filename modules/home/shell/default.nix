4cdd45b7c53e2780cea2c28011611dd25e7eca3a
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-12-11 12:14:56 +0000

    Feat: Added yazi's shell wrapper

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -20,6 +20,15 @@
   programs.bash = {
     enable = true;
     enableCompletion = true;
+    initExtra = ''
+      function y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        yazi "$@" --cwd-file="$tmp"
+        IFS= read -r -d '\' cwd < "$tmp"
+        [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+        rm -f -- "$tmp"
+      }
+    '';
   };
 
   # Software
