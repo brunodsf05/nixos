6c86e0f8b3088b3a86c80cdab6680daf29e7a15b
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-27 13:58:27 +0100

    Fix: Added missing pipe-operator feature to first rebuild in installer

--- install.sh
+++ install.sh
@@ -95,7 +95,7 @@ printf "\r\033[K\033[36mInstalling $chosen!\033[0m\n"
 echo
 
 sudo \
-  NIX_CONFIG="experimental-features = nix-command flakes" \
+  NIX_CONFIG="experimental-features = nix-command flakes pipe-operators" \
   nix-shell -p git --run \
   "nixos-rebuild switch --flake \"$REPO_SRC#$chosen\""
 
