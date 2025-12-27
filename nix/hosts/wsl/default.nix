25b9a9debb673b966b1de17ff3cdcb0b84a77bde
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-27 15:51:58 +0000

    Refactor: Merge wsl/system.nix into wsl/default.nix

--- nix/hosts/wsl/default.nix
+++ nix/hosts/wsl/default.nix
@@ -11,6 +11,16 @@
     inputs.nixos-wsl.nixosModules.default
     inputs.home-manager.nixosModules.home-manager
     ./modules/home
-    ./modules/system.nix
   ];
+
+  system.stateVersion = "25.05";
+
+  wsl.enable = true;
+  wsl.defaultUser = "nixos";
+  wsl.interop.includePath = false;
+
+  my.nixos.nh.enable = true;
+  my.host.main_user.name = "nixos";
+  my.hello.enable = true;
+  my.vscode_remote.enable = true;
 }
