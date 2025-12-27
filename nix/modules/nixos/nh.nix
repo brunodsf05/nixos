0cb17030abc8bf7c570dbdc9c88fa0255ec115ef
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-27 14:56:09 +0000

    Feat: Module to enable NH with the flake path automatically

---
+++ nix/modules/nixos/nh.nix
@@ -0,0 +1,18 @@
+{ config, global, lib, pkgs, ... }:
+
+let
+  inherit (global.fun.mkModule __curPos.file config) cfg cfgRoot wrapInModule;
+in
+{
+  options = wrapInModule {
+    enable = lib.mkEnableOption "NH nix-wrapper with an already set flake path";
+  };
+
+  config = lib.mkIf cfg.enable
+  {
+    programs.nh = {
+      enable = true;
+      flake = "/home/${cfgRoot.host.main_user.name}/nixos";
+    };
+  };
+}
