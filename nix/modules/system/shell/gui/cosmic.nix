d5faaddbbe450c15d5197d104f70cc4b687c168e
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-28 17:32:27 +0000

    Feat: Module to enable cosmic desktop

---
+++ nix/modules/system/shell/gui/cosmic.nix
@@ -0,0 +1,18 @@
+{ config, global, inputs, lib, pkgs, ... }:
+
+let
+  inherit (global.fun.mkModule __curPos.file config) cfg wrapInModule;
+in
+{
+  options = wrapInModule {
+    enable = lib.mkEnableOption "cosmic desktop";
+  };
+
+  config = lib.mkIf cfg.enable
+  {
+    services.displayManager.cosmic-greeter.enable = true;
+
+    services.desktopManager.cosmic.enable = true;
+    services.desktopManager.cosmic.xwayland.enable = true;
+  };
+}
