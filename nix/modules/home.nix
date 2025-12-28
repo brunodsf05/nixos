86323698fb7f9b883e5f5a7a0649270d6cece691
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-28 13:17:09 +0000

    Feat: Expose mainUser home.imports

--- nix/modules/home.nix
+++ nix/modules/home.nix
@@ -7,10 +7,17 @@ in
 {
   options = wrapInModule {
     enable = lib.mkEnableOption "HomeManager for the mainUser";
+
     stateVersion = lib.mkOption {
       type = lib.types.str;
       default = "";
       description = "Sets the value for `home-manager.users.\${mainUser}.home.stateVersion`. Set and keep the value to the release of HM you've installed.";
+    };
+
+    imports = lib.mkOption {
+      type = lib.types.listOf lib.types.deferredModule;
+      default = [];
+      description = "Home Manager modules imported into the mainUser.";
     };
   };
 
@@ -20,6 +27,7 @@ in
     home-manager.useUserPackages = true;
 
     home-manager.users.${mainUser} = {
+      imports = cfg.imports;
       home.stateVersion = cfg.stateVersion;
       programs.home-manager.enable = true;
     };
