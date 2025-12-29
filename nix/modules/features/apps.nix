1c927d5583926315b5c4c727923e153051c6f580
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-12-29 23:03:04 +0000

    Feat(module): Created apps module to add my apps

---
+++ nix/modules/features/apps.nix
@@ -0,0 +1,25 @@
+{ config, global, lib, pkgs, ... }:
+
+let
+  inherit (global.fun.mkModule __curPos.file config) cfg wrapInModule;
+in
+{
+  options = wrapInModule {
+    enable = lib.mkEnableOption "my apps";
+  };
+
+  config = lib.mkIf cfg.enable
+  {
+    environment.systemPackages = with pkgs; [
+      brave
+      gearlever
+      gimp
+      inkscape
+      keepassxc
+      qbittorrent
+      stremio
+      thunderbird
+      upscaler
+    ];
+  };
+}
