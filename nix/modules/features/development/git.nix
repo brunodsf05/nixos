b0631590aa478698e3ddaf29542e2c02cd4c10ed
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-01-12 14:35:44 +0000

    Feat(module): Git includes "authors" alias to list all authors user names, emails and number of commits

--- nix/modules/features/development/git.nix
+++ nix/modules/features/development/git.nix
@@ -18,6 +18,7 @@ in
             init.defaultBranch = "main";
             user.name = "brunodsf05";
             user.email = "231746160+brunodsf05@users.noreply.github.com";
+            alias.authors = "shortlog -s -n -e";
           };
         };
 
