c57e84f10b313b971be4fc838c9ae6f5185c4b5b
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-02-03 14:25:06 +0000

    Refactor(host.vm): Sorted my.nix lines

--- nix/hosts/vm/my.nix
+++ nix/hosts/vm/my.nix
@@ -1,25 +1,25 @@
 { config, global, inputs, lib, pkgs, ... }:
 
 {
+  my.system = {
+    host = {
+      main_user = {
+        config = {
+          extraGroups = [ "networkmanager" "wheel" ];
+          isNormalUser = true;
+        };
+        name = "bruno";
+      };
+    };
+
+    platform = {
+      home.stateVersion = "25.11";
+      nixos.system.stateVersion = "25.11";
+    };
+  };
+
   my.variables = {
     has.cli = true;
     has.gui = true;
-  };
-
-  my.system = {
-    platform = {
-      nixos.system.stateVersion = "25.11";
-      home.stateVersion = "25.11";
-    };
-
-    host = {
-      main_user = {
-        name = "bruno";
-        config = {
-          isNormalUser = true;
-          extraGroups = [ "networkmanager" "wheel" ];
-        };
-      };
-    };
   };
 }
