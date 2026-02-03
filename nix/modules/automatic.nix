61571d835839003c49fad12f6aec885cc294967f
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-02-03 10:32:12 +0000

    Docs(module): Prettified automatic.nix

--- nix/modules/automatic.nix
+++ nix/modules/automatic.nix
@@ -7,12 +7,17 @@ in
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
 
+  ### SYSTEM ###
   my.system.platform.nixos.nh.enable = lib.mkDefault true;
   my.system.platform.home.enable = lib.mkDefault true;
+
   my.system.shell.cli.enable = lib.mkDefault true;
   my.system.shell.gui.enable = lib.mkDefault var.has.gui;
+
   my.system.environment.locale.enable = lib.mkDefault true;
   my.system.environment.software.executable.enable = lib.mkDefault true;
+
+  ### FEATURES ###
   my.features.development.editor.cli.enable = true;
   my.features.development.git.enable = lib.mkDefault true;
   my.features.development.tooling.enable = true;
