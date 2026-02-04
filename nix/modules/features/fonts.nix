a44f877750e741f1869997120d0b8b487dad4ce0
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2026-02-04 14:17:41 +0100

    Fix(module): Fonts are correctly installed

--- nix/modules/features/fonts.nix
+++ nix/modules/features/fonts.nix
@@ -10,7 +10,7 @@ in
 
   config = lib.mkIf cfg.enable
   {
-    environment.systemPackages = [
+    fonts.packages = [
       pkgs.nerd-fonts.cousine # terminal
       pkgs.nerd-fonts.jetbrains-mono # editor
     ];
